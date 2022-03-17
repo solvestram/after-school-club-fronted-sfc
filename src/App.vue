45c87bfe5d5f80685090914a0d1caab5de56ecf9
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2022-03-17 13:04:02 +0000

    imported and registered lesson and checkout components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
+import lesson from './components/LessonsList.vue';
+import checkout from './components/CheckoutForm.vue';
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: { lesson, checkout },
+};
 </script>
+
+<style>
+</style>
