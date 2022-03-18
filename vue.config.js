e793c688fc450b71a593aeb9d5a6273fc54a33a0
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2022-03-18 10:05:00 +0000

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === "production" ? "/after-school-club-fronted-sfc/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/after-school-club-fronted-sfc/" : "/",
+
+  devServer: {
+    proxy: '*',
+  }
 })
